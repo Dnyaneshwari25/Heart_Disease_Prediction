6aed7f867936ae53c067abb6ea138c35df60a705
Dnyaneshwari25 <64651882+Dnyaneshwari25@users.noreply.github.com>
2022-03-19 23:25:30 +0530

    Update heart.py

--- model_codes/heart.py
+++ model_codes/heart.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import joblib
 
-df = pd.read_csv("../data/heart.csv")
+df = pd.read_csv("..\data\heart.csv")
 
 categorical_val = []
 continous_val = []
